247ebfb5722217ea3ffab3ec7795c736c70db32c
Akdes Simamora <amlosiorh1@gmail.com>
2023-04-24 18:15:26 +0700

    change

--- Applikasi_Toko_Bahagia/be/toko-bahagia-app/resources/views/layouts/scripts.blade.php
+++
@@ -1,8 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.6/dist/sweetalert2.all.min.js"></script>
-<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
-<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
-<script src="{{ asset('js/method.js') }}"></script>
-<script src="{{ asset('js/plugin.js') }}"></script>
-<script src="{{ asset('assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
-@yield('scripts')
